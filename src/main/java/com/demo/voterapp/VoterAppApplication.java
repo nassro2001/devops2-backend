1f1b9f1ad3c8af04262df39ceece8a5c98d01ffd
mehdi Nassereddine <nassromehdi128@gmail.com>
2025-05-07 22:46:15 +0300

    hello

---
+++ src/main/java/com/demo/voterapp/VoterAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo.voterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoterAppApplication.class, args);
+    }
+
+}
